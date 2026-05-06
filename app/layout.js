4fa46a84523bbca47a33760fae757c2a5fb09712
lusf1218 <lusf1218@gmail.com>
2026-05-06 17:08:26 +0800

    add layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+
+export const metadata = {
+  title: "婚禮桌位查詢",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-Hant">
+      <body>{children}</body>
+    </html>
+  );
+}
